be6c4adcf5754e850c24a10d6c03e1b7f543ec42
Minh Quan Dang <minh.quan040501@gmail.com>
2023-03-03 14:41:22 +0700

    Add fake header

--- demo_scrapy/spiders/coingecko.py
+++ demo_scrapy/spiders/coingecko.py
@@ -2,7 +2,7 @@ import scrapy
 import time
 from demo_scrapy.items import CoinGeckoCrawlerItem
 import time
-
+import random
 
 class CoingeckoSpider(scrapy.Spider):
     urlSeen = []
@@ -10,6 +10,13 @@ class CoingeckoSpider(scrapy.Spider):
     allowed_domains = ['www.coingecko.com']
     start_urls = ['https://www.coingecko.com']
     current_page = 1
+    user_agent_list = [
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36',
+        'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1',
+        'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 Edg/87.0.664.75',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.18363',
+    ]
     # rules = (
     #     Rule(LinkExtractor(allow='/en/coins/'), callback='parse_item_news'),
     #     Rule(LinkExtractor(allow='/?page'), follow=True, )
@@ -29,13 +36,13 @@ class CoingeckoSpider(scrapy.Spider):
         for item_url in urls:
             # print(item_url)
             time.sleep(1)
-            yield scrapy.Request(response.urljoin(item_url), callback=self.parse_coin)
+            yield scrapy.Request(response.urljoin(item_url), callback=self.parse_coin, headers={"User-Agent": self.user_agent_list[random.randint(0, len(self.user_agent_list)-1)]})
 
         # has_next_page = 'page-item next' == response.css("ul.pagination > li.page-item:nth-last-child(1)").xpath('@class').extract_first()
         if self.current_page <= 3:
             print('\n\n\n\n\n' + str(self.current_page))
             self.current_page += 1
-            yield scrapy.Request(response.urljoin('/?page=' + str(self.current_page)), callback=self.parse)
+            yield scrapy.Request(response.urljoin('/?page=' + str(self.current_page)), callback=self.parse, headers={"User-Agent": self.user_agent_list[random.randint(0, len(self.user_agent_list)-1)]})
 
     def parse_coin(self, response):
         item = CoinGeckoCrawlerItem()
